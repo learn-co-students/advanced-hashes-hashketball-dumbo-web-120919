1efaf0ae32ab39143e486f1e988170c10b364489
Miguel Ben <itsmigben@gmail.com>
2019-12-09 13:43:23 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -208,12 +208,6 @@ def big_shoe_rebounds
   
 end
 
-# # Bonus Questions
-
-# # Since there are multiple bonus questions that ask me
-# # to return the name of a player with the most of some stat, I can use the following methods
-# # to DRY (don't repeat yourself) up my code.
-
 def iterate_through_players_for(name, statistic)
   
   game_hash.each do |_team, game_data|
@@ -258,9 +252,6 @@ def most_points_scored
 end
 
 def winning_team
-  # Set up a hash to keep track of the points scored by each team. This way, we
-  # can iterate through each player, get their points scored, and increase the
-  # count in the hash.
 
   scores = { 'Brooklyn Nets' => 0, 'Charlotte Hornets' => 0 }
 
